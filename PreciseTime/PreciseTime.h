ce677f525f99c6845669a3deb594860651f4d3dd
Stéphane Copin <stephane.copin@live.fr>
2016-05-17 13:07:07 -0700

    Initial Commit

---
+++ PreciseTime/PreciseTime.h
@@ -0,0 +1,19 @@
+//
+//  PreciseTime.h
+//  PreciseTime
+//
+//  Created by Stéphane Copin on 5/17/16.
+//  Copyright © 2016 Stéphane Copin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PreciseTime.
+FOUNDATION_EXPORT double PreciseTimeVersionNumber;
+
+//! Project version string for PreciseTime.
+FOUNDATION_EXPORT const unsigned char PreciseTimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PreciseTime/PublicHeader.h>
+
+
